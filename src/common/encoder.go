d24f2f33922443f0ab36b755e11426dad4bdd758
rochelle <graciasrochelle@github.com>
2020-06-29 14:29:23 +1000

    allow cors for client domain

--- src/common/encoder.go
+++ src/common/encoder.go
@@ -14,6 +14,9 @@ func WriteWithPayload(w http.ResponseWriter, payload ResponseType) {
 
 func EncodeJsonResponse(w http.ResponseWriter, status int, response ResponseType) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
+	w.Header().Set("Access-Control-Allow-Origin", "https://graciasrochelle.github.io")
 	w.WriteHeader(status)
 	resp, e := json.Marshal(response)
 	if e != nil {
