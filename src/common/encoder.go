b153c15e7f2607ac83f49dc544c0282558f674e3
rochelle <graciasrochelle@github.com>
2020-06-29 23:24:43 +1000

    Update encoder.go

--- src/common/encoder.go
+++ src/common/encoder.go
@@ -13,10 +13,10 @@ func WriteWithPayload(w http.ResponseWriter, payload ResponseType) {
 }
 
 func EncodeJsonResponse(w http.ResponseWriter, status int, response ResponseType) error {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
-	w.Header().Set("Access-Control-Allow-Origin", "http://graciasrochelle.github.io")
+	w.Header().Set("Content-Type", "application/json;")
+	w.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS,GET,PATCH, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,crossdomain, mode")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(status)
 	resp, e := json.Marshal(response)
 	if e != nil {
